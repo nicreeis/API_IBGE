256e308ca2b320d47b32b8f8e03a74ecd598ec6c
Nicole Reis <nicolereismkt@gmail.com>
2023-10-24 09:03:40 -0300

    Create ibge_code.py

---
+++ ibge_code.py
@@ -0,0 +1,29 @@
+import requests
+import pandas as pd
+import numpy as np
+
+def obter_quantidade_sexo(nome, sexo):
+    url = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}?sexo={sexo}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        df = pd.json_normalize(data[0]["res"])
+        total = df["frequencia"].sum()
+        return total
+    return None
+
+def obter_sexo_correto(nome):
+    sexo_M = obter_quantidade_sexo(nome,"M")
+    sexo_F = obter_quantidade_sexo(nome,"F")
+    if sexo_M > sexo_F:
+        return "Masculino"
+    else:
+        return "Feminino"
+
+df_dados_clientes = pd.read_csv(r'C:\Users\nfrei\Downloads\dataset_clientes.csv')
+
+df_dados_clientes['Sexo Corrigido'] = df_dados_clientes['Nome'].apply(obter_sexo_correto)
+
+df_dados_clientes['Sexo está correto?'] = np.where(df_dados_clientes['Sexo'] == df_dados_clientes['Sexo Corrigido'] , 'True','False')
+
+df_dados_clientes.to_csv(r'C:\Users\nfrei\Desktop\dados_clientes_corrigido.csv', index=False)
